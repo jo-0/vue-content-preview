48205b8fe42facb63e81ae6241d652201d1e825d
Ajo <17722952+jo-0@users.noreply.github.com>
2020-12-09 19:43:24 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="container">
+		<div class="card-container">
+			<card
+				v-for="user in users"
+				:key="user.id"
+				:user-name="user.name"
+				:website="user.website"
+				@click.native="showPreview(user)"
+			/>
+		</div>
+		<preview />
+	</div>
+</template>
+
+<script>
+import Card from "@/components/card";
+import Users from "@/users";
+import Preview from "@/components/preview";
+
+export default {
+	name: "App",
+	components: {
+		Card,
+		Preview
+	},
+	data() {
+		return {
+			users: Users.users,
+			isPreviewActive: false
+		};
+	},
+	methods: {
+		showPreview(user) {
+			console.log(user.name);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	display: flex;
+	margin: 20px 60px;
+}
+
+.card-container {
+	display: flex;
+	flex-wrap: wrap;
+	margin: 20px 60px;
+}
+</style>
---
+++ src/components/preview.vue
@@ -0,0 +1,47 @@
+<template>
+	<section class="preview">
+		<p>IMG</p>
+		<div>
+			<header>Heading</header>
+			<p>29 july</p>
+			<div>
+				<h4>Summary</h4>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown
+					printer took a galley of type and scrambled it to make a
+					type specimen book. It has survived not only five centuries,
+					but also the leap into electronic typesetting, remaining
+					essentially unchanged.
+				</p>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown
+					printer took a galley of type and scrambled it to make a
+					type specimen book. It has survived not only five centuries,
+					but also the leap into electronic typesetting, remaining
+					essentially unchanged. It was popularised in the 1960s with
+					the release of Letraset sheets containing Lorem Ipsum
+					passages, and more recently with desktop publishing software
+					like Aldus PageMaker including versions of Lorem Ipsum.
+				</p>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "preview"
+};
+</script>
+
+<style lang="scss" scoped>
+.preview {
+    border: 1px solid black;
+    border-radius: 12px;
+    padding: 20px;
+}
+</style>
